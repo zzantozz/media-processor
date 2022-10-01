1fbd4ef996562f2c9adb7ffcb4bca1fda5060e8c
ryan <zzantozz@gmail.com>
2022-10-01 13:54:21 -0500

    fully working scanner script
    The previous version of the script only contained a function to run on
    each file. This pulls over the commands I was running to get the files
    and tidies up the script a bit.
    
    It'll now build an initial set of "create" events from a directory,
    ignoring files that it's seen before.

--- scanner.sh
+++ scanner.sh
@@ -3,31 +3,40 @@ fun() {
 
 sha1="$(sha1sum "$1" | cut -d ' ' -f 1)" 
 now="$(date +%s)"
+# File paths containing a single quote (') mess up the insert. This would be a good reason to move to a language that
+# supports prepared statements. We can also just escape them by replacing every ' with ''.
 path="${1//\'/\'\'}"
-cur_stmt="select * from file_events where file_path = '$path';"
-cur="$(sqlite test.db "$cur_stmt")"
-IFS='|' read -r -a cur_fields <<< "$cur"
-if [[ "${cur_fields[0]}" = created ]]; then
-  echo "setting stmt1"
-  stmt="foobie!"
+# Does this file exist yet?
+find_current_stmt="select * from file_events where file_path = '$path';"
+current="$(sqlite test.db "$find_current_stmt")"
+IFS='|' read -r -a current_fields <<< "$current"
+if [ -n "$current" ]; then
+  echo "file path already exists. what next?"
 else
-  echo "setting stmt2"
-  stmt="insert into file_events(type, file_path, time, storage_location, sha1) values('created', '$path', $now, 'Amazon', '$sha1');"
+  echo "file path not tracked, adding create event"
+  stmt="insert into file_events(event_type, file_path, time, storage_location, sha1) "
+  stmt+="values('created', '$path', $now, 'Amazon', '$sha1');"
 fi
 if [[ "$DEBUG" = true ]]; then
   echo " -- $1"
   echo "path: $path"
   echo "sha1: $sha1"
   echo "updated: $now"
-  echo "current state: ${cur_fields[*]}"
+  echo "current state: ${current_fields[*]}"
   echo "stmt: $stmt"
 fi
-attempts=0
-while ! sqlite test.db "$stmt" &> /dev/null; do
-  attempts=$((attempts+1))
-  if [ $attempts -ge 2 ]; then
-    echo "Insert attempt $attempts failed, retrying..."
-    sleep 1
-  fi
-done
+if [ -n "$stmt" ]; then
+  attempts=0
+  # When I query the db while this is running, it can cause locking problems. If the insert fails, sleep and retry.
+  while ! sqlite test.db "$stmt" &> /dev/null; do
+    attempts=$((attempts+1))
+    if [ $attempts -ge 2 ]; then
+      echo "Insert attempt $attempts failed, retrying..."
+      sleep 1
+    fi
+  done
+fi
 }
+export -f fun
+export AMAZON_DIR=/mnt/d/amazon-drive/Amazon\ Drive/
+find "$AMAZON_DIR" -type f -print0 | xargs -0 -I {} bash -c 'fun "{}"'
