490169aeea676ebc0cc026bd55bce591764c4012
ryan <zzantozz@gmail.com>
2022-10-01 14:15:40 -0500

    better debug output for next phase
    It's inserting "create" events for newly-found files. Now it's time to
    start looking for changed files. This takes the rudimentary debug output
    and organizes it better so I can move forward more easily.

--- scanner.sh
+++ scanner.sh
@@ -1,31 +1,38 @@
-fun() {
-: ${DEBUG:=false}
+#!/bin/bash
 
-sha1="$(sha1sum "$1" | cut -d ' ' -f 1)" 
+: "${DEBUG:=false}"
+
+debug() {
+  [ "$DEBUG" = true ] && echo "$1"
+}
+export -f debug
+
+fun() {
+debug " -- $1"
+sha1="$(sha1sum "$1" | cut -d ' ' -f 1)"
+debug "calculated sha1: $sha1"
 now="$(date +%s)"
+debug "time: $now"
 # File paths containing a single quote (') mess up the insert. This would be a good reason to move to a language that
 # supports prepared statements. We can also just escape them by replacing every ' with ''.
 path="${1//\'/\'\'}"
+debug "db-safe path: $path"
 # Does this file exist yet?
-find_current_stmt="select * from file_events where file_path = '$path';"
+find_current_stmt="select event_type, sha1 from file_events where file_path = '$path' order by time desc limit 1;"
 current="$(sqlite test.db "$find_current_stmt")"
 IFS='|' read -r -a current_fields <<< "$current"
+debug "current state: ${current_fields[*]}"
 if [ -n "$current" ]; then
-  echo "file path already exists. what next?"
+  debug "file path already exists. what next?"
+  debug "event_type: ${current_fields[0]}"
+  debug "sha1: ${current_fields[1]}"
 else
-  echo "file path not tracked, adding create event"
+  debug "file path not tracked, adding create event"
   stmt="insert into file_events(event_type, file_path, time, storage_location, sha1) "
   stmt+="values('created', '$path', $now, 'Amazon', '$sha1');"
 fi
-if [[ "$DEBUG" = true ]]; then
-  echo " -- $1"
-  echo "path: $path"
-  echo "sha1: $sha1"
-  echo "updated: $now"
-  echo "current state: ${current_fields[*]}"
-  echo "stmt: $stmt"
-fi
 if [ -n "$stmt" ]; then
+  debug "stmt: $stmt"
   attempts=0
   # When I query the db while this is running, it can cause locking problems. If the insert fails, sleep and retry.
   while ! sqlite test.db "$stmt" &> /dev/null; do
