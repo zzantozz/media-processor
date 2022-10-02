65d082e25eb3a7ff76a8e299896fe2102c6e8ed9
Ryan <rds6235@gmail.com>
2022-10-02 14:43:40 -0500

    add create schema sql file
    The scripts I'm working on here use a sqlite db. This adds a script for
    creating the schema expected by the scripts.

---
+++ create-schema.sql
@@ -0,0 +1,15 @@
+-- Creates the db schema expected by the scripts here.
+--
+-- Usage:
+--   sqlite test.db < create-schema.sql
+
+-- Creates an events table to store discrete file events, like "create", "update", and "delete". Rows should be treated
+-- as immutable because they just represent discrete events at a point in time.
+create table file_events (
+    file_path text not null,
+    time integer not null,
+    event_type text not null,
+    storage_location text not null,
+    sha1 text not null, -- might not know sha1 on delete, but we can look it up from most recent event
+    primary key (file_path, time)
+);
