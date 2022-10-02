011854c0a4d8f40e5610b6da9bd31a7bf67d2525
Ryan <rds6235@gmail.com>
2022-10-02 14:48:14 -0500

    add db audit script
    Since everything right now is very dynamic, it would be really easy for
    typos to creep in and corrupt the database. The first thing I noticed
    was that the event type is one of a few (three right now) distinct
    values, and if some script uses a bad value, like "created" instead of
    "create", then everything falls over.
    
    This attempts to address the issue by adding a script that can audit the
    db and be sure that everything looks as expected. For now, it only looks
    at event type. Add other checks as the need becomes apparent.

---
+++ audit-db.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+echo "Searches the db for rows that violate some constraint, meaning that YOU CODED SOMETHING WRONG!"
+echo ""
+
+violations=0
+
+res="$(sqlite test.db "select * from file_events where event_type not in ('create', 'update', 'delete');")"
+[ -n "$res" ] && {
+  echo -e " *** Here are the rows with an invalid 'event_type':\n$res"
+  violations=$((violations+1))
+}
+
+echo "Found $violations areas with violations"
+[ $violations -eq 0 ] || exit 1
